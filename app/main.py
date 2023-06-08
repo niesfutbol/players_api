b31eaa827e03f7217d5f9a1c84a571925ec07d81
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-06-07 19:54:06 -0700

    🔥 Delete old queries

--- app/main.py
+++ app/main.py
@@ -10,25 +10,11 @@ PLAYERS = pfa.get_player_info_from_list(players["response"])
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-@app.get("/make_add")
-def make_add():
-    return {"add": 4 + 6}
-
-
-@app.get("/all_bd")
-def get_all_items():
-    base_datos = pd.read_csv(PATH_DATABASE)
-    return base_datos.to_dict()
-
 @app.get("/player/{player_id}")
 def get_player(player_id: int):
     return PLAYERS[player_id]
+
+
+@app.get("./players")
+def get_player():
+    return pfa.get_player_and_id(players["response"])
