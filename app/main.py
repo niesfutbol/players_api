bc6406319b32de5c99fa0dac817095b1ba7deacf
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-06-10 16:18:37 -0700

    🚧 Add header

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 import json
 import players_from_as as pfa
 
@@ -6,15 +7,16 @@ import players_from_as as pfa
 f = open("./players.json")
 players = json.load(f)
 PLAYERS = pfa.get_player_info_from_list(players["response"])
+headers = {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Credentials": "true"}
 
 app = FastAPI()
 
 
-@app.get("/player/{player_id}")
-def get_player(player_id: int):
-    return PLAYERS[player_id]
+@app.get("/v1")
+def get_player(player: int = 54):
+    return JSONResponse(content=PLAYERS[player], headers=headers)
 
 
-@app.get("/players")
+@app.get("/v1/players")
 def get_player():
-    return pfa.get_player_and_id(players["response"])
+    return JSONResponse(content=pfa.get_player_and_id(players["response"]), headers=headers)
