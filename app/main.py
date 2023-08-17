ca8f203d293000f1378aff52e869e15abff01126
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-08-16 17:13:50 -0700

    🚧 Add endpoint

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 import json
+import pandas as pd
 import players_from_as as pfa
 
 
-f = open("./players.json")
+f = open("./data/players.json")
+large = pd.read_csv("./data/larga_player.csv")
 players = json.load(f)
 PLAYERS = pfa.get_player_info_from_list(players["response"])
 headers = {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Credentials": "true"}
@@ -20,3 +22,8 @@ def get_player(player: int = 54):
 @app.get("/v1/players")
 def get_player():
     return JSONResponse(content=pfa.get_player_and_id(players["response"]), headers=headers)
+
+
+@app.get("/v1/percentile")
+def get_player():
+    return JSONResponse(content=large.to_dict(orient = "records"), headers=headers)
